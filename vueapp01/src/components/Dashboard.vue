8ece94ea0e8828998c82bda2228459f278883eae
Piyush <you@example.com>
2017-05-29 20:05:26 +0530

    Vue project

---
+++ vueapp01/src/components/Dashboard.vue
@@ -0,0 +1,43 @@
+<template>
+	<div class="container">
+		<Header />
+		<Aside />
+		 <input v-model="newTodo" />
+		 <button v-on:click="addTodo">Add</button>
+		<Content
+			v-bind:todos="todos"
+      v-bind:deletetodo="deletetodo"
+      v-bind:complete="complete"
+		 />
+	</div>
+</template>
+
+<script>
+import Header from './Header'
+import Aside from './Aside'
+import Content from './Content'
+
+export default {
+  methods: {
+    addTodo: function () { if (this.newTodo) this.todos.splice(0, 0, { id: this.todos.length + 1, text: this.newTodo, completed: false }); this.newTodo = undefined },
+    deletetodo: function (id) { console.log(id); this.index = this.todos.findIndex(x => x.id === id); this.todos.splice(this.index, 1) },
+    complete: function (todo) { todo.completed = true }
+  },
+  data () {
+    return {
+      msg: 'this is msg part',
+      newTodo: undefined,
+      todos: []
+    }
+  },
+  components: {
+    Header, Content, Aside
+  }
+}
+</script>
+
+<style scoped>
+	.container {
+		padding: 10px;
+	}
+</style>
---
+++ vueapp01/src/router/index.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Login from '@/components/Login'
+import Dashboard from '@/components/Dashboard'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'login',
+      component: Login
+    },
+    {
+      path: '/dashboard',
+      component: Dashboard
+    }
+  ]
+})
