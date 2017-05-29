0ee44daf20cc23462b7376859223dd1b5f276416
Piyush <you@example.com>
2017-05-29 10:32:05 +0530

    Modified drf

--- drf/mysite/apps/serializers.py
+++ drf/mysite/apps/serializers.py
@@ -1,23 +1,49 @@
 from rest_framework import serializers
 from django.db import models
 from .models import User
+from rest_framework.response import Response
+from rest_framework import status
 
-class UserSerializer(serializers.Serializer):
+class UserSerializer(serializers.ModelSerializer):
     first_name = serializers.CharField()
     last_name = serializers.CharField()
     id = serializers.UUIDField()
 
     class Meta:
-        model = User
+        model = User # we need to tell which model we are using
+        fields = ('first_name', 'last_name', 'id')
+    #
+    # def create(self, validated_data):
+    #     return User.objects.create(**validated_data)
+    #
+    # def update(self, instance, validated_data):  # for put method
+    #     """
+    #     Update and return an existing `Snippet` instance, given the validated data.
+    #     """
+    #     instance.first_name = validated_data.get('first_name', instance.first_name)
+    #     instance.last_name = validated_data.get('last_name', instance.last_name)
+    #     instance.save()
+    #     return instance
 
-    def create(self, validated_data):
-        return User.objects.create(**validated_data)
+    def validate_first_name(self, data):
+        """
+        Check that the start is before the stop.
+        """
+        nv=0
+        val= self.initial_data['last_name']
+        for i in val:
+            raise serializers.ValidationError(val)
 
-    def update(self, instance, validated_data):
-        """
-        Update and return an existing `Snippet` instance, given the validated data.
-        """
-        instance.first_name = validated_data.get('first_name', instance.first_name)
-        instance.last_name = validated_data.get('last_name', instance.last_name)
-        instance.save()
-        return instance
+        for i in data:
+            raise serializers.ValidationError(data)
+            if i in 'adiouAEIOU':
+                nv = nv + 1
+                if nv > 0:
+                    validate_last_name()
+                    raise serializers.ValidationError('error, vowel in first name')
+
+
+        return data
+
+
+
--- drf/mysite/apps/urls.py
+++ drf/mysite/apps/urls.py
@@ -1,9 +1,25 @@
 from django.conf.urls import url
 from rest_framework.urlpatterns import format_suffix_patterns
+from rest_framework import routers
+from .views import UserViewSet
 from . import views
 
-urlpatterns = [
-    url(r'^users/$', views.user_list),
-    url(r'^users/(?P<pk>[0-9a-z\-]+)$',views.user_detail),
-]
-urlpatterns = format_suffix_patterns(urlpatterns)
+# urlpatterns = [
+#     url(r'^users/$', views.user_list),
+#     url(r'^users/(?P<pk>[0-9a-z\-]+)$',views.user_detail),
+# ]
+
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
+urlpatterns = router.urls
+
+# url (r/users , 'UserViewSet.as_views(
+#  "get" : "list"
+#  "post" : "create" })
+
+#   url (r/users/<pk> , 'UserViewSet.as_views(
+#  "get" : "retrieve" retrieeve return a single object hence in mixins.py the <pk> is passed internally
+#  "post" : "update" })
+#
+#
+# urlpatterns = format_suffix_patterns(urlpatterns)
